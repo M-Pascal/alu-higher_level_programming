6fe445d10140420ed9b6013da2a7ef0ca6a859b6
Pascal <p.mugisha@alustudent.com>
2023-07-15 03:59:57 -0700

    agashya_2

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # 10-divisible_by_2.py
 
+
 def divisible_by_2(my_list=[]):
     multiples = []
     for i in range(len(my_list)):
--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 # 8-multiple_returns.py
+
+
 def multiple_returns(sentence):
     if sentence == "":
         return (0, None)
