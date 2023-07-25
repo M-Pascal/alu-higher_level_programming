b61dc8f6de510c215a3dc281411d6e486c0d17a4
Pascal <p.mugisha@alustudent.com>
2023-07-25 13:54:53 -0700

    Walk of life

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def magic_string():
-    
-        return f"BestSchool".split(",")
-    
+def magic_string(n):
+    return "".join(["BestSchool" + str(i) for i in range(1, n + 1)])
