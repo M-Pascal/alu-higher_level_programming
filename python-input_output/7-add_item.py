08876ff410f509043cb09568dcb72085e971cdc0
Pascal <p.mugisha@alustudent.com>
2023-07-27 11:05:56 -0700

    fame

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 # script that adds all arguments to a Python list,
 # and then save them to a file
-
-
-
+"""
+    script that adds all arguments to a Python list,
+    and save them to a file
+"""
 
 import sys
 
