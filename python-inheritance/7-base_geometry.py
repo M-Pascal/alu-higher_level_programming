7565ae4e704be4240664e710673ec1e34b7b6cc6
Pascal <p.mugisha@alustudent.com>
2023-07-27 23:15:42 -0700

    ma

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""
-a python script that raises an error if the value is not an integer
-"""
+"""Defines a base geometry class BaseGeometry."""
 
 
-class BaseGeometry:
-    """
-    ...........
-    """
+class BaseGeometry:    
+    """Reprsent base geometry."""
+
     def area(self):
+        """Not yet implemented."""        
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError("name must be an integer")
+        """Validate a parameter as an integer.
+        :param name: name of the parameter
+        :param value: name of parameter to validator
+        :returns TypeError if value is not an interger
+        :returns ValueError if value is <= 0>
+        """
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("age must be greater than 0")
-        self.name = name
-        self.value = value
+            raise ValueError("{} must be greater than 0".format(name))
