a8ab04f06408ae816b407ba0477ba9ca66394bca
Pascal <p.mugisha@alustudent.com>
2023-07-27 23:04:36 -0700

    ma

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-'''Geometry class'''
+"""
+a python script that raises an error if the value is not an integer
+"""
 
 
 class BaseGeometry:
-    '''Geometry class'''
+    """
+    ...........
+    """
     def area(self):
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
-        if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+        if not isinstance(value, int):
+            raise TypeError("name must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("age must be greater than 0")
+        self.name = name
+        self.value = value
