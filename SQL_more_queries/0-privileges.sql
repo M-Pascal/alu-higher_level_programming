a5d5ee1c993011b9a78dd2d35a841d96e841d7f7
Pascal <p.mugisha@alustudent.com>
2023-08-01 05:25:59 -0700

    Loading...

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,5 @@
--- Lists all privileges of the MySQL users
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- show grants for two users
+-- show grants for user_0d_1
+SHOW GRANTS FOR user_0d_1@localhost;
+-- show grants for user_0d_2
+SHOW GRANTS FOR user_0d_2@localhost;
--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
--- creates the MySQL server user_0d_1 and grant all priviledges
+-- create a user on server and assign all privileges
+-- create user
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+-- assign all privileges to everything
 GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
