e3f65579ebc75c6bbc5e040a22dfd81c414b992c
Pascal <p.mugisha@alustudent.com>
2023-08-01 05:35:15 -0700

    Loading...

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,15 +1,3 @@
-#!/bin/bash
-
 # MySQL server credentials
-MYSQL_USER="your_mysql_username"
-MYSQL_PASS="your_mysql_password"
-
-# MySQL user names for privileges listing
-USERS=("user_0d_1" "user_0d_2")
-
-# Loop through the user names and list their privileges
-for user in "${USERS[@]}"; do
-    echo "Privileges for user: $user"
-    mysql -u"$MYSQL_USER" -p"$MYSQL_PASS" -e "SHOW GRANTS FOR '$user'@'localhost';"
-    echo "-------------------------"
-done
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
