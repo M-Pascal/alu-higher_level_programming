5b50ef5a8a28212e14dcb9d5de4f67cec0f09819
Pascal <p.mugisha@alustudent.com>
2023-08-01 03:46:18 -0700

    queries...

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,6 @@
--- creates the MySQL server user_0d_1 and grant all priviledges
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+-- create user
+CREATE USER IF NOT EXISTS user_0d_1@localhost
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL
+ON *.*
+TO user_0d_1@localhost;
