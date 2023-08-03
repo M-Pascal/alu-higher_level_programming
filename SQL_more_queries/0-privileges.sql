7fb127edc899eb3f0db5738f3cbc8fbfcdf3356a
Pascal <p.mugisha@alustudent.com>
2023-08-03 04:20:06 -0700

    Please abeg

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,6 @@
 -- privileges!
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
+CREATE USER 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- privileges for user_0d_2
+CREATE USER 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
