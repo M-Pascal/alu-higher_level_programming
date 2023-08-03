d91b0c6f8606e250c368c5cdb8f5bc467ce5069d
Pascal <p.mugisha@alustudent.com>
2023-08-03 15:21:16 -0700

    Abeg...

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,23 @@
--- Creating user with privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+-- Connect to the MySQL server using a privileged user (e.g., root)
+-- Replace 'your_root_username' and 'your_root_password' with the actual credentials
+
+-- Step 1: Check if the user already exists
+SELECT COUNT(*) INTO @userExists FROM mysql.user WHERE user = 'user_0d_1';
+
+-- Step 2: Create or update the user
+SET @query = '';
+IF @userExists > 0 THEN
+    -- User already exists, update the password
+    SET @query = CONCAT('ALTER USER \'user_0d_1\'@\'localhost\' IDENTIFIED WITH mysql_native_password BY \'user_0d_1_pwd\';');
+ELSE
+    -- User does not exist, create the user with all privileges
+    SET @query = CONCAT('CREATE USER \'user_0d_1\'@\'localhost\' IDENTIFIED WITH mysql_native_password BY \'user_0d_1_pwd\';');
+END IF;
+
+-- Step 3: Grant all privileges to the user
+SET @query = CONCAT(@query, 'GRANT ALL PRIVILEGES ON *.* TO \'user_0d_1\'@\'localhost\';');
+
+-- Step 4: Apply the changes
+PREPARE stmt FROM @query;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
