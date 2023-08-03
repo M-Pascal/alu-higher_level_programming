318c4f126d8003a413088095275a8abe08530424
Pascal <p.mugisha@alustudent.com>
2023-08-03 02:31:09 -0700

    Bitinze

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -2,9 +2,8 @@
 
 -- Step 1: Create the user 'user_0d_1'@'localhost'
 CREATE USER 'user_0d_1'@'localhost';
-
--- Step 2: Grant all privileges to 'user_0d_1'@'localhost'
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
--- Step 3: Check the granted privileges for 'user_0d_1'@'localhost'
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- Step 2: Grant all privileges to 'user_0d_1'@'localhost'
+CREATE USER 'user_0d_2'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
