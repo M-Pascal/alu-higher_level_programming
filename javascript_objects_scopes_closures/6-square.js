bb6c252b5530bec9a6e3f29eaec56125f7309489
Pascal <p.mugisha@alustudent.com>
2023-10-01 05:40:59 -0700

    Task_6-10

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -2,7 +2,7 @@
 module.exports = class Square extends require('./5-square') {
   charPrint (c) {
     const char = c || 'X';
-    for (let i = 0; i < this.height; i++) {
+    for (let i = 0; i < this.height; i++){
       console.log(char.repeat(this.width));
     }
   }
