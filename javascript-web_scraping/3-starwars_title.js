f0d084c023e25905ce7312f462483ca6acafc50d
M-Pascal <p.mugisha@alustudent.com>
2023-10-07 08:02:48 +0000

    Amending...

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,30 +1,22 @@
 #!/usr/bin/node
 
-const https = require('https');
+const request = require('request');
 
 if (process.argv.length !== 3) {
-  console.error('Usage: script.js <movie_id>');
+  console.error('Usage: ./3-starwars_title.js <movie_id>');
   process.exit(1);
 }
 
 const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-https.get(url, (res) => {
-  let data = '';
-
-  res.on('data', (chunk) => {
-    data += chunk;
-  });
-
-  res.on('end', () => {
-    if (res.statusCode === 200) {
-      const movie = JSON.parse(data);
-      console.log(`Episode ${movie.episode_id}: ${movie.title}`);
-    } else {
-      console.error(`Movie with ID ${movieId} not found.`);
-    }
-  });
-}).on('error', (err) => {
-  console.error('Error:', err.message);
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
+  } else {
+    console.error(`Movie with ID ${movieId} not found.`);
+  }
 });
