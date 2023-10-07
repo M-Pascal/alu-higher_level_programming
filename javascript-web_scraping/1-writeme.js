beb88398fca1668a0588e2282f11502ce67c8f8d
M-Pascal <p.mugisha@alustudent.com>
2023-10-07 08:26:53 +0000

    Amending...

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,28 +1,19 @@
 #!/usr/bin/node
-const { argv } = require('process');
+
 const fs = require('fs');
 
-
-function closeFd (fd) {
-  fs.close(fd, (err) => {
-    if (err) console.log(err);
-  });
+if (process.argv.length !== 4) {
+  console.error('Usage: ./1-writeme.js <file_path> <string_to_write>');
+  process.exit(1);
 }
 
+const filePath = process.argv[2];
+const content = process.argv[3];
 
-if (argv[2]) {
-  fs.open(argv[2], 'w', (err, fd) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    fs.write(fd, argv[3], (err, written, string) => {
-      if (err) {
-        console.log(err);
-      }
-      closeFd(fd);
-    });
-  });
-} else {
-  console.log('An error occurred no file(path) was given');
-}
+fs.writeFile(filePath, content, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`Content has been written to ${filePath}`);
+  }
+});
